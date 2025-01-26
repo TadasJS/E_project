64b5f9824732553404f90b64375f1ccb64f15bdc
Tadas <tadas.x@gmail.com>
2025-01-26 23:22:52 +0200

    created server and client with eslint env. created connection to postgresDB

---
+++ server/eslint.config.js
@@ -0,0 +1,54 @@
+import eslintPluginPrettier from "eslint-plugin-prettier";
+import eslintConfigPrettier from "eslint-config-prettier";
+
+import pkg from "@eslint/js";
+const { configs: eslintRecommended } = pkg;
+
+import prettierRecommended from "eslint-plugin-prettier/recommended";
+
+export default [
+  eslintRecommended.recommended,
+
+  prettierRecommended,
+
+  eslintConfigPrettier,
+
+  {
+    files: ["**/*.js", "**/*.mjs"],
+
+    plugins: {
+      prettier: eslintPluginPrettier,
+    },
+
+    languageOptions: {
+      globals: {
+        console: "readonly",
+        process: "readonly",
+      },
+    },
+
+    rules: {
+      "prettier/prettier": "error",
+
+      "no-console": "off",
+
+      eqeqeq: "error",
+
+      curly: "error",
+
+      "comma-dangle": ["error", "always-multiline"],
+
+      "comma-spacing": ["error", { before: false, after: true }],
+
+      "comma-style": ["error", "last"],
+
+      indent: ["error", 2],
+
+      quotes: ["error", "double"],
+
+      semi: ["error", "always"],
+
+      "object-curly-spacing": ["error", "always"],
+    },
+  },
+];
---
+++ server/postgresdb/postgresConnection.mjs
@@ -0,0 +1,29 @@
+import dotenv from 'dotenv'
+import pg from 'pg';
+
+dotenv.config({path: '.env.dev'});
+
+const { Pool } = pg;
+
+// console.log(pg)
+
+const pool = new Pool({
+  user: process.env.POSTGRES_USER,
+  host: process.env.POSTGRES_HOST,
+  database: process.env.POSTGRES_DB,
+  password: process.env.POSTGRES_PASSWORD,
+  port: process.env.POSTGRES_PORT,
+})
+
+export async function connectDB() {
+    try {
+        await pool.connect()
+       console.log('Database connected successfully');
+    } catch (error) {
+        console.error(error.stack);
+        throw(error);
+    }
+};
+
+
+
