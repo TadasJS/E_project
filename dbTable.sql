8e291bfefaaa15210bbbc1578d2380a6cfa80c2a
Tadas <tadas.x@gmail.com>
2025-02-07 10:00:04 +0200

    data base created

--- dbTable.sql
+++ dbTable.sql
@@ -1,4 +1,3 @@
-
 CREATE TABLE users (
 id SERIAL PRIMARY KEY,
 user_name VARCHAR(20) NOT NULL,
@@ -30,43 +29,48 @@ VALUES ('user', 'user@user.com'), ('admin', 'admin@admin.com');
 INSERT INTO users_secrets (userId, password, roleId)
 VALUES ( 1, '$2b$13$54L2rigIHP5ea23svcITFe01eX5TsQIXy3.dM/61lm.dQx9T.XPRu', 1), (2,'$2b$13$6mk62x4oUSpQi37HGhOMmecre29Yzv4pH7WOCVkzwSS.Z1Dq2q.7y', 2);
 
-CREATE TABLE procedure(
+CREATE TABLE category(
 id SERIAL PRIMARY KEY NOT NULL, 
-name varchar(50) NOT NULL, 
-category varchar(50) NOT NULL,
-time TIME NOT NULL,
-foto varchar(50) NOT NULL,
+name varchar(30) NOT NULL, 
 created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP);
 
-CREATE TABLE registration(
+CREATE TABLE advert(
 id SERIAL PRIMARY KEY NOT NULL,
-reg_date DATE NOT NULL, 
-reg_time TIME NOT NULL,
-verified BOOL NOT NULL,
-new_registration BOOL NOT NULL,
+title varchar(50) NOT NULL,
+description varchar (100) NOT NULL,
+price NUMERIC (9,2) NOT NULL,
+photo varchar (100) NOT NULL,
 user_id INT NOT NULL,
-procedure_id INT NOT NULL,
+category_id INT NOT NULL,
 created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 FOREIGN KEY (user_id) REFERENCES users (id),
-FOREIGN KEY (procedure_id) REFERENCES procedure (id));
+FOREIGN KEY (category_id) REFERENCES category (id));
 
+CREATE TABLE comments(
+id SERIAL PRIMARY KEY,
+comment VARCHAR(200) NOT NULL,
+user_id INT NOT NULL,
+advert_id INT NOT NULL,
+created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+FOREIGN KEY (user_id) REFERENCES users (id),
+FOREIGN KEY (advert_id) REFERENCES advert (id));
 
-
-
-CREATE TABLE rating (
-  id SERIAL PRIMARY KEY NOT NULL,
-  proc_rating INT NOT NULL,
-  procedure_id INT NOT NULL,
+CREATE TABLE is_like (
+  id SERIAL PRIMARY KEY,
+  like_is BOOL NOT NULL,
+  user_id INT NOT NULL,
+  advert_id INT NOT NULL,
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  FOREIGN KEY (procedure_id) REFERENCES procedure (id));
+  FOREIGN KEY (user_id) REFERENCES users (id),
+  FOREIGN KEY (advert_id) REFERENCES advert (id));
 
 
-INSERT INTO procedure (name, category, time, foto)
-VALUES ('mot plauku kirpimas', 'Plauku kirpimas', '01:00', 'no foto'),
-('vyr plauku kirpimas', 'Plaukuf kirpimas', '00:30', 'foto url');
+INSERT INTO category (name)
+VALUES ('zaislai'), ('baldai'), ('automobiliai');
 
-INSERT INTO registration (reg_date, reg_time, verified, new_registration, user_id, procedure_id)
-VALUES ('2020.12.12', '12:00', '0', '0', 1, 1);
+INSERT INTO advert (title, description, price, photo, user_id, category_id )
+VALUES ('zaisliukas', 'geras zaisliukas', 3.5, 'image_url', 1, 1 ),
+('baldai', 'geri baldai', 20.5, 'image_url', 1, 2 ),
+('masina', 'geras masina', 3000, 'image_url', 1, 3 );
