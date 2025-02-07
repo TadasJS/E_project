3e4e2158a521b128c1f9f51fca4cee9081c69973
Tadas <tadas.x@gmail.com>
2025-02-07 11:31:55 +0200

    Advert crud created

--- server/routes/index.mjs
+++ server/routes/index.mjs
@@ -1,18 +1,16 @@
 import express from "express";
 import { users } from "./usersRegisterRoutes.mjs";
 import { login } from "./userLoginRoutes.mjs";
-import { procedure } from "./procedureRoutes.mjs";
-import { procedReg } from "./procedRegRoutes.mjs";
-import { rating } from "./procedRatingRoutes.mjs";
+import { advert } from "./advert.mjs";
+
 
 const api = express.Router();
 
 api.use("/user/login", login);
 api.use("/user/register", users);
 
-api.use("/procedure", procedure);
-api.use('/procedreg', procedReg);
-api.use('/rating', rating);
+api.use("/advert", advert);
+
 
 api.use("/", (req, res) => {
   res.status(200).json({ status: "ok", msg: " API SERVER : page not created yet" });
